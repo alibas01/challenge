a821f45a8a4794a21989ff01639666bc7cb594f3
Ali Bas <alibas01@gmail.com>
2021-07-04 19:18:58 -0400

    queens attack third solution

--- queensAttack.js
+++ queensAttack.js
@@ -10,7 +10,6 @@
  *  5. 2D_INTEGER_ARRAY obstacles (array of arrays. formed by 2 integer that represents seperate obstacle location. [[row, column],[row, column]] obstacles.length = k)
  */
 
-
 //-------------------------------First Answer---------------
 // const queensAttack = function(n, k, rq, cq, obstacles) {
 //   // Write your code here
@@ -88,56 +87,115 @@
 //   return result - 1;
 // };
 
-
 //-------------------second answer------------------
-const queensAttack = function (n, k, rq, cq, obstacles) {
-  // Write your code here
-  let result = 0;
-  for (let i = 1; i <= n; i++) {
-    // row
-    for (let j = 1; j <= n; j++) {
-      //column
-      if (i === rq && j !== cq) {
-        result += 1;
-        if (k > 0) {
-          obstacles.forEach((a) => {
-            if (
-              (a[0] === rq && a[1] > cq && j >= a[1]) ||
-              (a[0] === rq && a[1] < cq && j <= a[1])
-            )
-              result -= 1;
-          });
-        }
+// const queensAttack = function (n, k, rq, cq, obstacles) {
+//   // Write your code here
+//   let result = 0;
+//   for (let i = 1; i <= n; i++) {
+//     // row
+//     for (let j = 1; j <= n; j++) {
+//       //column
+//       if (i === rq && j !== cq) {
+//         result += 1;
+//         if (k > 0) {
+//           obstacles.forEach((a) => {
+//             if (
+//               (a[0] === rq && a[1] > cq && j >= a[1]) ||
+//               (a[0] === rq && a[1] < cq && j <= a[1])
+//             )
+//               result -= 1;
+//           });
+//         }
+//       }
+//       if (j === cq && i !== rq) {
+//         result += 1;
+//         if (k > 0) {
+//           obstacles.forEach((a) => {
+//             if (
+//               (a[1] === cq && a[0] > rq && i >= a[0]) ||
+//               (a[1] === cq && a[0] < rq && i <= a[0])
+//             )
+//               result -= 1;
+//           });
+//         }
+//       }
+//       if (Math.abs(i - rq) === Math.abs(j - cq)) {
+//         result += 1;
+//         if (k > 0) {
+//           obstacles.forEach((a) => {
+//             if (Math.abs(a[0] - rq) === Math.abs(a[1] - cq)) {
+//               if (i >= a[0] && j >= a[1] && a[0] > rq && a[1] > cq) result -= 1;
+//               if (i <= a[0] && j <= a[1] && a[0] < rq && a[1] < cq) result -= 1;
+//               if (i >= a[0] && j <= a[1] && a[0] > rq && a[1] < cq) result -= 1;
+//               if (i <= a[0] && j >= a[1] && a[0] < rq && a[1] > cq) result -= 1;
+//             }
+//           });
+//         }
+//       }
+//     }
+//   }
+//   return result - 1;
+// };
+//--------------------third answer-------------
+function position(row, col) {
+  if (row < col) {
+    return row;
+  } else {
+    return col;
+  }
+}
+function queensAttack(n, k, r_q, c_q, obstacles) {
+  let result =
+    2 * (n - 1) +
+    position(n - c_q, n - r_q) +
+    position(c_q - 1, r_q - 1) +
+    position(n - c_q, r_q - 1) +
+    position(n - r_q, c_q - 1);
+
+  if (obstacles) {
+    for (let z = 0; z < obstacles.length; z++) {
+      // for evry obstacle
+      r_o = obstacles[z][0];
+      c_o = obstacles[z][1];
+      // if in the same row
+      if (r_o === r_q && c_o > c_q) {
+        result -= n - c_o + 1;
       }
-      if (j === cq && i !== rq) {
-        result += 1;
-        if (k > 0) {
-          obstacles.forEach((a) => {
-            if (
-              (a[1] === cq && a[0] > rq && i >= a[0]) ||
-              (a[1] === cq && a[0] < rq && i <= a[0])
-            )
-              result -= 1;
-          });
-        }
+      if (r_o === r_q && c_o < c_q) {
+        result -= c_o;
       }
-      if (Math.abs(i - rq) === Math.abs(j - cq)) {
-        result += 1;
-        if (k > 0) {
-          obstacles.forEach((a) => {
-            if (Math.abs(a[0] - rq) === Math.abs(a[1] - cq)) {
-              if (i >= a[0] && j >= a[1] && a[0] > rq && a[1] > cq) result -= 1;
-              if (i <= a[0] && j <= a[1] && a[0] < rq && a[1] < cq) result -= 1;
-              if (i >= a[0] && j <= a[1] && a[0] > rq && a[1] < cq) result -= 1;
-              if (i <= a[0] && j >= a[1] && a[0] < rq && a[1] > cq) result -= 1;
-            }
-          });
+
+      // if in the same column
+      if (c_o === c_q && r_o > r_q) {
+        result -= n - r_o + 1;
+      }
+      if (c_o === c_q && r_o < r_q) {
+        result -= r_o;
+      }
+
+      // if diaogonal
+      if (Math.abs(r_o - r_q) === Math.abs(c_o - c_q)) {
+        // if upper right sector
+        if (r_o > r_q && c_o > c_q) {
+          result -= position(n - r_o + 1, n - c_o + 1);
+        }
+        // if down left sector
+        if (r_o < r_q && c_o < c_q) {
+          result -= position(r_o, c_o);
+        }
+        // if upper left sector
+        if (r_o > r_q && c_o < c_q) {
+          result -= position(n - r_o + 1, c_o);
+        }
+        // if right down sector
+        if (r_o < r_q && c_o > c_q) {
+          result -= position(r_o, n - c_o + 1);
         }
       }
     }
   }
-  return result - 1;
-};
+  return result;
+}
 
 //------------examples--------------
 console.log(
